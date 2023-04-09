3931b8d8a8555627f1d3f8caf4e9599d15bebada
Apestein <ltn2057@protonmail.com>
2023-04-09 01:51:37 -0400

    fix middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * - public folder
      */
-    "/((?!static|.*\\..*|_next|favicon.ico).*)",
-    "/",
+    "/((?!_next/image|_next/static|favicon.ico).*)",
   ],
 }
