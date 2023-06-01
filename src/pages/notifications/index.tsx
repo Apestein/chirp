b9f57f0a5178d97f3c6dd84c5ece3d2f01f745a7
Apestein <ltn2057@protonmail.com>
2023-05-31 22:53:41 -0400

    save point

---
+++ src/pages/notifications/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "~/components/layout"
+
+export default function NotificationsPage() {
+  return (
+    <Layout>
+      <main className="flex justify-center overflow-auto">
+        <div className="container relative h-fit min-h-full border-x border-[#ffffff50]"></div>
+      </main>
+    </Layout>
+  )
+}
